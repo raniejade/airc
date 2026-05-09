80e36fab57a3c4286f86f4a06d72bdab588c5a21
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2026-05-09 21:47:45 +1000

    M1: add reusable CLI renderer module under src/cli/output/

---
+++ src/cli/output/index.ts
@@ -0,0 +1,13 @@
+export { type ColorMode, type Style, detectColorMode, styles } from './color.js';
+export {
+  type ConfigWarningView,
+  type WarningSeverity,
+  renderDoctor,
+} from './doctor.js';
+export {
+  type InstallAction,
+  type InstallChangeView,
+  type InstallResultView,
+  renderInstall,
+} from './install.js';
+export { badge, pad, relPath, renderEmpty, renderList, renderSuccess, symbol } from './render.js';
